e908ee0f9cd99827ec6e309523ba4cde2c949497
Mohamad Aldulemi <mohamadaldulemi@gmail.com>
2026-03-25 19:18:12 +0100

    email header analyzer spf done

---
+++ headerchecks/spf_check.py
@@ -0,0 +1,57 @@
+import spf
+import re
+
+def extrahiere_absender_ip(headers):
+    
+    received_headers = [
+        h["value"] for h in headers if h["name"].lower() == "received"
+    ]
+
+    #Von unten nach oben also die erste ip von unten diese brauche ich
+    for header in reversed(received_headers):
+        #Ip Adresse auf dem Header extrahieren
+        #IPv4
+        match = re.search(r'\[(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\]', header)
+        if match:
+            return match.group(1) #Reine IP ohne die Klammer
+
+        # IPv6
+        match = re.search(r'\[([0-9a-fA-F:]+)\]', header)
+        if match:
+            return match.group(1)
+
+
+    return None
+
+def extrahiere_return_path(headers):
+    #Ich hole mir nur das erste Ergebnis, falls keines vorhanden ist einfach leeren String ausgeben
+    return_path = next(
+            (h["value"] for h in headers if h["name"].lower() == "return-path"),
+            ""
+    ) 
+
+    match = re.search(r'<(.+?)>', return_path)
+    if match:
+        return match.group(1) #Wieder die reine IP Adresse, also ohne die Klammern
+    return return_path   
+
+
+
+def prüfe_spf(headers): 
+    ip = extrahiere_absender_ip(headers) #Der Server der die Mail geschickt hat
+    absender = extrahiere_return_path(headers) #Die Adresse die beim SMTP angegeben wurde
+
+    if not ip or not absender:
+        return "none", "Konnte IP oder Absender nicht extrahieren"
+
+    domain = absender.split("@")[-1] 
+
+    #SPF check
+    result, explanation = spf.check2(
+        i = ip,
+        s = absender,
+        h = domain
+
+    )
+
+    return result, explanation
--- main.py
+++ main.py
@@ -7,9 +7,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 import time
-import spf
-import re
-
+from headerchecks import prüfe_spf
 print("Lade Modell...")
 modell = joblib.load("./models/phishing_model.pkl")
 print("Modell geladen")
@@ -56,62 +54,6 @@ def extrahiere_text(payload):
     return text
 
 
-def extrahiere_absender_ip(headers):
-    
-    received_headers = [
-        h["value"] for h in headers if h["name"].lower() == "received"
-    ]
-
-    #Von unten nach oben also die erste ip von unten diese brauche ich
-    for header in reversed(received_headers):
-        #Ip Adresse auf dem Header extrahieren
-        #IPv4
-        match = re.search(r'\[(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\]', header)
-        if match:
-            return match.group(1) #Reine IP ohne die Klammer
-
-        # IPv6
-        match = re.search(r'\[([0-9a-fA-F:]+)\]', header)
-        if match:
-            return match.group(1)
-
-
-    return None
-
-def extrahiere_return_path(headers):
-    #Ich hole mir nur das erste Ergebnis, falls keines vorhanden ist einfach leeren String ausgeben
-    return_path = next(
-            (h["value"] for h in headers if h["name"].lower() == "return-path"),
-            ""
-    ) 
-
-    match = re.search(r'<(.+?)>', return_path)
-    if match:
-        return match.group(1) #Wieder die reine IP Adresse, also ohne die Klammern
-    return return_path   
-
-
-
-def prüfe_spf(headers): 
-    ip = extrahiere_absender_ip(headers) #Der Server der die Mail geschickt hat
-    absender = extrahiere_return_path(headers) #Die Adresse die beim SMTP angegeben wurde
-
-    if not ip or not absender:
-        return "none", "Konnte IP oder Absender nicht extrahieren"
-
-    domain = absender.split("@")[-1] 
-
-    #SPF check
-    result, explanation = spf.check2(
-        i = ip,
-        s = absender,
-        h = domain
-
-    )
-
-    return result, explanation
-
-
 
 def prüfe_mails(service, modell):
     results = service.users().messages().list(
@@ -130,8 +72,7 @@ def prüfe_mails(service, modell):
         ).execute()
 
         headers = mail_data["payload"]["headers"]
-        for h in headers:
-             print(f"{h['name']}: {h['value']}")
+       
         betreff = next((h["value"] for h in headers if h["name"] == "Subject"), "")
         absender = next((h["value"] for h in headers if h["name"] == "From"), "")
 
